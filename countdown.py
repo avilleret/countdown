ab3b10cab37328c8cb2298013def1cba9aee1234
Antoine Villeret <antoine.villeret@gmail.com>
2017-10-29 12:40:11 +0100

    update server

--- countdown.py
+++ countdown.py
@@ -6,6 +6,7 @@ import time
 import apa102
 import logging.handlers
 import os
+import datetime
 
 """
 This script create an OSCQuery device that control an APA102 LED strip to make an interactive countdown.
@@ -53,12 +54,12 @@ parameter_text = node.create_parameter(ossia.ValueType.String)
 parameter_text.value = "HELP"
 
 node = local_device.add_node("/speed")
-parameter = node.create_parameter(ossia.ValueType.Float)
-parameter.access_mode = ossia.AccessMode.Bi
-parameter.bounding_mode = ossia.BoundingMode.Clip
-parameter.make_domain(-2,2)
-parameter.apply_domain()
-parameter.value = 1
+parameter_speed = node.create_parameter(ossia.ValueType.Float)
+parameter_speed.access_mode = ossia.AccessMode.Bi
+parameter_speed.bounding_mode = ossia.BoundingMode.Clip
+parameter_speed.make_domain(-10,10)
+parameter_speed.apply_domain()
+parameter_speed.value = 1
 
 node = local_device.add_node("/brightness")
 parameter = node.create_parameter(ossia.ValueType.Int)
@@ -76,30 +77,55 @@ parameter_play.value = 0
 node = local_device.add_node("/time")
 parameter_time = node.create_parameter(ossia.ValueType.Float)
 parameter_time.access_mode = ossia.AccessMode.Get
+node.refresh_rate = 200
 parameter_time.value = 0
+
+node = local_device.add_node("/blink/delay")
+parameter_blink_del = node.create_parameter(ossia.ValueType.Float)
+parameter_blink_del.access_mode = ossia.AccessMode.Bi
+parameter_blink_del.value = 1000.
+
+node = local_device.add_node("/blink/on")
+parameter_blink_del = node.create_parameter(ossia.ValueType.Bool)
+parameter_blink_del.access_mode = ossia.AccessMode.Bi
+parameter_blink_del.value = False
 
 globq = ossia.GlobalMessageQueue(local_device)
 
 # Setup LED
 # 6 LED / segments, 7 segments per digit, 4 digits + 4 points to separate min and seconds
-# 6 * 7 * 4 + 4  172 LEDS
-strip = apa102.APA102(172, 2)
+# 6 * 7 * 4 + 8 = 176 LEDS
+strip = apa102.APA102(176, 2)
 
 time_flag = True
+alast_time = datetime.datetime.now()
+print("started on " + str(alast_time) )
 
 def update_led_display():
+  if time_flag:
+    minutes = int(parameter_time.value/60)
+    seconds = int(parameter_time.value%60)
+    #print("time : " + str(minutes) + ":" + str(seconds))
   strip.show()
 
 def update_time():
+  if parameter_play.value:
+    if parameter_time.value > 0:
+      now = datetime.datetime.now()
+      delta = now - alast_time
+      parameter_time.value -= delta.total_seconds() * parameter_speed.value
+      print(parameter_time.value)
+
+def set_time():
   s = parameter_text.value
   try:
-    minutes = int(float(s[:1]))
+    minutes = int(float(s[:2]))
     seconds = int(float(s[-2:]))
     parameter_time.value = minutes*60 + seconds
+    print("set_time: " + str(minutes) + ":" + str(seconds))
   except:
-    #print("update_time: " + minutes + ":" + seconds)
     print("can't parse text to time")
-    parameter_play.value = 0
+    parameter_play.value = False
 
 
 while(True):
@@ -108,10 +134,12 @@ while(True):
   while res != None:
     parameter, value = res
     if str(parameter.node) == "/text":
-      update_time()
+      set_time()
     print("globq: Got " +  str(parameter.node) + " => " + str(value))
     res=globq.pop()
 
+  update_time()
+  alast_time = datetime.datetime.now()
   update_led_display()
   time.sleep(0.01)
 

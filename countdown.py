f3abc60534fd5e49caa80138c5528d40d2802acd
Antoine Villeret <antoine.villeret@gmail.com>
2017-11-07 02:10:16 +0100

    add test fn

--- countdown.py
+++ countdown.py
@@ -15,99 +15,104 @@ import signal
 This script create an OSCQuery device that control an APA102 LED strip to make an interactive countdown.
 """
 
+# Configure logging to log to a file, making a new file at midnight and keeping the last 3 day's data
+LOG_LEVEL = logging.DEBUG
+LOG_FILENAME = os.path.dirname(os.path.realpath(__file__)) + "/countdown.log"
+print("logfile: " + LOG_FILENAME)
+# Give the logger a unique name (good practice)
+logger = logging.getLogger(__name__)
+# Set the log level to LOG_LEVEL
+logger.setLevel(LOG_LEVEL)
+# Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
+handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
+# Format each log message like this
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+# Attach the formatter to the handler
+handler.setFormatter(formatter)
+# Attach the handler to the logger
+logger.addHandler(handler)
+
+# Replace stdout with logging to file at INFO level
+#sys.stdout = MyLogger(logger, logging.INFO)
+# Replace stderr with logging to file at ERROR level
+#sys.stderr = MyLogger(logger, logging.ERROR)
+
+# setup OSCQuery device
+local_device = ossia.LocalDevice("countdown")
+print("local device name: " + local_device.name)
+
+local_device.create_oscquery_server(1234, 5678, False)
+
+# create a node, create a float parameter, set its properties and initialize it
+node = local_device.add_node("/color")
+parameter_color = node.create_parameter(ossia.ValueType.Vec3f)
+parameter_color.access_mode = ossia.AccessMode.Bi
+parameter_color.bounding_mode = ossia.BoundingMode.Clip
+parameter_color.make_domain(0,255)
+parameter_color.apply_domain()
+parameter_color.value = [255,0,0]
+
+node = local_device.add_node("/text")
+parameter_text = node.create_parameter(ossia.ValueType.String)
+parameter_text.value = "HELP"
+
+node = local_device.add_node("/speed")
+parameter_speed = node.create_parameter(ossia.ValueType.Float)
+parameter_speed.access_mode = ossia.AccessMode.Bi
+parameter_speed.bounding_mode = ossia.BoundingMode.Clip
+parameter_speed.make_domain(-10,10)
+parameter_speed.apply_domain()
+parameter_speed.value = 1
+
+node = local_device.add_node("/brightness")
+parameter_brightness = node.create_parameter(ossia.ValueType.Int)
+parameter_brightness.access_mode = ossia.AccessMode.Bi
+parameter_brightness.bounding_mode = ossia.BoundingMode.Clip
+parameter_brightness.make_domain(0,31)
+parameter_brightness.apply_domain()
+parameter_brightness.value = 1
+
+node = local_device.add_node("/play")
+parameter_play = node.create_parameter(ossia.ValueType.Bool)
+parameter_play.access_mode = ossia.AccessMode.Bi
+parameter_play.value = 0
+
+node = local_device.add_node("/time")
+parameter_time = node.create_parameter(ossia.ValueType.Float)
+parameter_time.access_mode = ossia.AccessMode.Get
+node.refresh_rate = 200
+parameter_time.value = 0
+
+# TODO : make blink work
+node = local_device.add_node("/blink/delay")
+parameter_blink_del = node.create_parameter(ossia.ValueType.Float)
+parameter_blink_del.access_mode = ossia.AccessMode.Bi
+parameter_blink_del.value = 1000.
+
+node = local_device.add_node("/blink/on")
+parameter_blink_on = node.create_parameter(ossia.ValueType.Bool)
+parameter_blink_on.access_mode = ossia.AccessMode.Bi
+parameter_blink_on.value = False
+
+node = local_device.add_node("/admin/update")
+parameter = node.create_parameter(ossia.ValueType.Impulse)
+
+node = local_device.add_node("/admin/reboot")
+parameter = node.create_parameter(ossia.ValueType.Impulse)
+
+node = local_device.add_node("/admin/test")
+parameter_led_test = node.create_parameter(ossia.ValueType.Bool)
+parameter_led_test.access_mode = ossia.AccessMode.Bi
+parameter_led_test.value = False
+
+globq = ossia.GlobalMessageQueue(local_device)
+
+# Setup LED
+# 6 LED / segments, 7 segments per digit, 4 digits + 4 points to separate min and seconds
+# 6 * 7 * 4 + 8 = 176 LEDS
+strip = apa102.APA102(176, 2)
+
 try:
-  # Configure logging to log to a file, making a new file at midnight and keeping the last 3 day's data
-  LOG_LEVEL = logging.DEBUG
-  LOG_FILENAME = os.path.dirname(os.path.realpath(__file__)) + "/countdown.log"
-  print("logfile: " + LOG_FILENAME)
-  # Give the logger a unique name (good practice)
-  logger = logging.getLogger(__name__)
-  # Set the log level to LOG_LEVEL
-  logger.setLevel(LOG_LEVEL)
-  # Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
-  handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
-  # Format each log message like this
-  formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
-  # Attach the formatter to the handler
-  handler.setFormatter(formatter)
-  # Attach the handler to the logger
-  logger.addHandler(handler)
-
-  # Replace stdout with logging to file at INFO level
-  #sys.stdout = MyLogger(logger, logging.INFO)
-  # Replace stderr with logging to file at ERROR level
-  #sys.stderr = MyLogger(logger, logging.ERROR)
-
-  # setup OSCQuery device
-  local_device = ossia.LocalDevice("countdown")
-  print("local device name: " + local_device.name)
-
-  local_device.create_oscquery_server(1234, 5678, False)
-
-  # create a node, create a float parameter, set its properties and initialize it
-  node = local_device.add_node("/color")
-  parameter_color = node.create_parameter(ossia.ValueType.Vec3f)
-  parameter_color.access_mode = ossia.AccessMode.Bi
-  parameter_color.bounding_mode = ossia.BoundingMode.Clip
-  parameter_color.make_domain(0,255)
-  parameter_color.apply_domain()
-  parameter_color.value = [255,0,0]
-
-  node = local_device.add_node("/text")
-  parameter_text = node.create_parameter(ossia.ValueType.String)
-  parameter_text.value = "HELP"
-
-  node = local_device.add_node("/speed")
-  parameter_speed = node.create_parameter(ossia.ValueType.Float)
-  parameter_speed.access_mode = ossia.AccessMode.Bi
-  parameter_speed.bounding_mode = ossia.BoundingMode.Clip
-  parameter_speed.make_domain(-10,10)
-  parameter_speed.apply_domain()
-  parameter_speed.value = 1
-
-  node = local_device.add_node("/brightness")
-  parameter_brightness = node.create_parameter(ossia.ValueType.Int)
-  parameter_brightness.access_mode = ossia.AccessMode.Bi
-  parameter_brightness.bounding_mode = ossia.BoundingMode.Clip
-  parameter_brightness.make_domain(0,31)
-  parameter_brightness.apply_domain()
-  parameter_brightness.value = 1
-
-  node = local_device.add_node("/play")
-  parameter_play = node.create_parameter(ossia.ValueType.Bool)
-  parameter_play.access_mode = ossia.AccessMode.Bi
-  parameter_play.value = 0
-
-  node = local_device.add_node("/time")
-  parameter_time = node.create_parameter(ossia.ValueType.Float)
-  parameter_time.access_mode = ossia.AccessMode.Get
-  node.refresh_rate = 200
-  parameter_time.value = 0
-
-  # TODO : make blink work
-  node = local_device.add_node("/blink/delay")
-  parameter_blink_del = node.create_parameter(ossia.ValueType.Float)
-  parameter_blink_del.access_mode = ossia.AccessMode.Bi
-  parameter_blink_del.value = 1000.
-
-  node = local_device.add_node("/blink/on")
-  parameter_blink_on = node.create_parameter(ossia.ValueType.Bool)
-  parameter_blink_on.access_mode = ossia.AccessMode.Bi
-  parameter_blink_on.value = False
-
-  node = local_device.add_node("/admin/update")
-  parameter = node.create_parameter(ossia.ValueType.Impulse)
-
-  node = local_device.add_node("/admin/reboot")
-  parameter = node.create_parameter(ossia.ValueType.Impulse)
-
-  globq = ossia.GlobalMessageQueue(local_device)
-
-  # Setup LED
-  # 6 LED / segments, 7 segments per digit, 4 digits + 4 points to separate min and seconds
-  # 6 * 7 * 4 + 8 = 176 LEDS
-  strip = apa102.APA102(176, 2)
-
   def sighandler(signum, frame):
     strip.clearStrip()
     strip.cleanup()
@@ -221,6 +226,15 @@ try:
     else:
       display_string(parameter_text.value)
 
+      offset = 7*2*6
+
+      for i in range(8):
+        id = (offset+i) * 4
+        strip.leds[id] = 0b11100000
+        strip.leds[id+1] = 0
+        strip.leds[id+2] = 0
+        strip.leds[id+3] = 0
+
     strip.show()
 
   def update_time():
@@ -230,6 +244,7 @@ try:
         now = datetime.datetime.now()
         delta = now - alast_time
         parameter_time.value -= delta.total_seconds() * parameter_speed.value
+    alast_time = datetime.datetime.now()
 
   def set_time():
     global time_flag
@@ -275,17 +290,25 @@ try:
       elif str(parameter.node) == "/admin/reboot":
         print("I'm going to reboot now !")
         os.system("reboot")
-      if str(parameter.node) == "/time":
+      elif str(parameter.node) == "/time":
         time_flag = True
 
       print("globq: Got " +  str(parameter.node) + " => " + str(value))
       res=globq.pop()
 
-    update_time()
-    alast_time = datetime.datetime.now()
-    blink()
-    update_led_display()
-    time.sleep(0.01)
+    if parameter_led_test.value:
+      numPixels = 176
+      for j in range(numPixels): # Shift the start of the rainbow across the strip
+        for i in range(numPixels): # spread (or compress) one rainbow onto the strip
+            # For a faster shift, add more than 1 * j per loop (e.g. + 2 * j)
+            index = strip.wheel((((i << 8) // numPixels) + j*4) & 255)
+            strip.setPixelRGB(i, index);
+        strip.show() 
+    else:
+      blink()
+      update_time()
+      update_led_display()
+      time.sleep(0.01)
 
 except:  # Abbruch...
   print("Unexpected error:", sys.exc_info()[0])
